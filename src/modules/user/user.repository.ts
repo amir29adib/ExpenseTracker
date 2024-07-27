1671f667c49f3121e329720b5a37be7a9b95bf00
Amir <amir29adib1234@gmail.com>
2024-07-27 20:15:22 +0330

    define update method for user

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,8 +1,11 @@
 import { v4 } from "uuid";
 import { User } from "./model/user";
+import { DataSourceType } from "../../data-soure";
+import fs from "fs";
 
 interface IUserRepository {
   create(user: CreateUser): User;
+  update(id: string, user: UpdateUser): User | undefined;
   findByUsername(username: string): User | undefined;
   findById(id: string): User | undefined;
   getAll(): User[];
@@ -13,8 +16,12 @@ export interface CreateUser {
   password: string;
 }
 
+export interface UpdateUser {
+  password: string;
+}
+
 export class UserRepository implements IUserRepository {
-  private users: User[] = [];
+  private users: User[] = getUserDataSource("json");
 
   public create(user: CreateUser): User {
     const createdUser = { ...user, id: v4() };
@@ -22,15 +29,44 @@ export class UserRepository implements IUserRepository {
     return createdUser;
   }
 
+  public findById(id: string): User | undefined {
+    return this.users.find((item) => item.id === id);
+  }
+
   public findByUsername(username: string): User | undefined {
     return this.users.find((item) => item.username === username);
   }
 
-  public findById(id: string): User | undefined {
-    return this.users.find((item) => item.id === id);
+  public update(id: string, user: UpdateUser): User | undefined {
+    const updatedUser = { ...user };
+    const index = this.users.findIndex((user) => user.id === user.id);
+
+    if (index !== -1) {
+      this.users[index] = { ...this.users[index], ...updatedUser };
+    }
+
+    return this.findById(id);
   }
 
   public getAll(): User[] {
     return this.users;
   }
 }
+
+const getUserDataSource = (env: DataSourceType) => {
+  switch (env) {
+    case "json":
+      const jsonString = fs.readFileSync("../../myData.json", "utf-8");
+      return JSON.parse(jsonString);
+
+    case "array":
+      break;
+
+    case "db":
+      break;
+
+    default:
+      const jsonStringDefault = fs.readFileSync("../../myData.json", "utf-8");
+      return JSON.parse(jsonStringDefault);
+  }
+};
